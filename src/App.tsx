3658292ed66f158724fa619a001da463034e21a3
Malik Kilgore <malik199228@gmail.com>
2021-01-08 22:34:23 -0500

    Organized & replaced some files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './components/List'
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>any file</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main">
+        <List></List>
+      </main>
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function List() {
+    
+    return (
+        <div className="listRoot">
+            <p>
+            Edit <code>src/components/List.tsx</code> and save to reload.
+            <br></br>
+            Just like this. Nerds. React ez
+            </p>
+        </div>
+    );
+}
+
+export default List;
