979534cda506e19c2695aad9eecaf23fa21c4fc4
Malik Kilgore <malik199228@gmail.com>
2021-01-08 23:41:20 -0500

    Installed dndkit library.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function List() {
-    
+    const [game, setGame] = useState([
+        {
+            //React hook
+        },
+    ]);
+
     return (
         <div className="listRoot">
             <p>
-            Edit <code>src/components/List.tsx</code> and save to reload.
+                Edit <code>src/components/List.tsx</code> and save to reload.
             <br></br>
             Just like this. Nerds. React ez
             </p>
