6be71661c310a7a154aef2a2c8add6c132c6b20f
Malik Kilgore <malik199228@gmail.com>
2021-01-11 23:10:40 -0500

    Created basic layout for each ListItem.

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,7 +4,15 @@ import '../css/ListItem.css';
 function ListItem () {
     return (
         <div className="item-Root"> 
-            <div className="item-Main"> I am a list Item, hey fam.</div>
+            <div className="item-Main">
+                <img className="itemImage" alt="Video game cover"></img>
+                <h1 className="itemTitle">Game title here</h1>
+                <h3 className="itemSearch">Search Bar here</h3>
+                <p className="itemReview">Your review here</p>
+            </div>
+            <div className="item-Footer">
+                Trash button here
+            </div>
         </div>
     )
 }
