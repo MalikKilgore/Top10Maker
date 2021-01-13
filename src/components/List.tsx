7e518c3720467ce0c240826d151afe7c00b44557
Malik Kilgore <malik199228@gmail.com>
2021-01-12 20:59:18 -0500

    Installed DndKit and made each list item drag and droppable.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,26 +1,80 @@
-import React from "react";
-// { useState }
-import '../css/List.css';
-import ListItem from './ListItem'
+import React, { useState } from "react";
+import "../css/List.css";
+import ListItem from "./ListItem";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 
 function List() {
- /* const [game, setGame] = useState([
-    {
-      //React hook
-    },
-  ]); */
+  const [items, setItems] = useState([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+  ]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
 
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: "unique-id",
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
   return (
     <div className="list-Root">
       <div className="list-Header"></div>
       <div className="list-Main">
-        <ListItem></ListItem>
-        <ListItem></ListItem>
-        <ListItem></ListItem>
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
+          <SortableContext items={items} strategy={verticalListSortingStrategy}>
+            {items.map((id) => (
+              <ListItem key={id} id={id} />
+            ))}
+          </SortableContext>
+        </DndContext>
       </div>
       <div className="list-Footer"></div>
     </div>
   );
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 }
 
 export default List;
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import '../css/ListItem.css';
+//import {useDraggable} from '@dnd-kit/core';
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
 
-function ListItem () {
+function ListItem (props: any) {
+      const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+      } = useSortable({id: props.id});
+
+      const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+      };
+      
     return (
-        <div className="item-Root"> 
+        <div className="item-Root" 
+        ref={setNodeRef} style={style} {...attributes} {...listeners}> 
             <div className="item-Main">
                 <img className="itemImage" alt="Video game cover"></img>
                 <h1 className="itemTitle">Game title here</h1>
