a9854cedc535fc984c17576ac1cce7b7b9bbe8da
Malik Kilgore <malik199228@gmail.com>
2021-01-12 23:28:56 -0500

    Installed Axios. Generated Twitch API key, testing IGDB API.

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/ListItem.css';
-//import {useDraggable} from '@dnd-kit/core';
 import {useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities';
+import axios from 'axios'
 
 function ListItem (props: any) {
+    const [search, setSearch] = useState([``]);
+
       const {
         attributes,
         listeners,
@@ -17,6 +19,27 @@ function ListItem (props: any) {
         transform: CSS.Transform.toString(transform),
         transition,
       };
+      const axiosConfig = {
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            'Client-ID': '4a1ri5yfip3ikw2rxmpgqjmu51nok7',
+            'Authorization': `Bearer 1hb6wzs1cvaz2hc2z1uqkto5vw8cp7`,
+          }
+      };
+      const fields = {
+        limit: 10,
+      }
+
+      function gameRequest(){
+        return axios.post(`https://api.igdb.com/v4/games`, fields, axiosConfig)
+        .then((results) => {
+          console.log(results)
+        })
+        .catch((err) => {
+            console.log("Axios Error: ", err)
+        })
+      }
       
     return (
         <div className="item-Root" 
@@ -24,7 +47,7 @@ function ListItem (props: any) {
             <div className="item-Main">
                 <img className="itemImage" alt="Video game cover"></img>
                 <h1 className="itemTitle">Game title here</h1>
-                <h3 className="itemSearch">Search Bar here</h3>
+                <input className="itemSearch" onClick={gameRequest} placeholder="search games here..."></input>
                 <p className="itemReview">Your review here</p>
             </div>
             <div className="item-Footer">
