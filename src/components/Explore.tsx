faa80cefeef63ac5539d6ae219a7044327f1fff7
Malik Kilgore <malik199228@gmail.com>
2021-02-19 23:41:17 -0500

    Returned query now properly maps within React div.

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,30 +1,31 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import "../css/Explore.css";
 import axios from 'axios';
 
-function Explore(){
-    function populatePage(){
-
-        axios.get('http://top10maker.com/explore/lists').then((response) => {
-            const array = response.data
-            array.forEach(function(list:any){
-                const element = <div className="completeList">{list}</div>;
-                console.log(list)
-                ReactDOM.render(element, document.getElementById('explore-Main'));
-            })
-        })
-    }
+function Explore() {
+    //Stores Received lists in an array
+    const [returnedLists, setRenderLists] = useState([]);
 
     return (
         <div className="explore-Root">
             this is the explore page
             <button onClick={populatePage}>Populate Page</button>
             <div className="explore-Main">
-
+                {returnedLists.map((list: any) => <div key={list._id} className={'completeList'}>
+                    {list.title}
+                    <br></br>
+                    {list.date}
+                </div>)}
             </div>
         </div>
     )
+    function populatePage() {
+        axios.get('http://top10maker.com/explore/lists').then((response) => {
+            //Response is an array of objects. The Objects are individual lists
+            const array = response.data
+            setRenderLists(array)
+        })
+    }
 }
 
 export default Explore
